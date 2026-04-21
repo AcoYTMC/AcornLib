cb574b14fa490764ad8f312b09cc6ff37e95fbb5
AcoYT <167049722+AcoYTMC@users.noreply.github.com>
2026-04-20 17:06:51 -0700

    new build (1.21.1-r22)

--- src/main/java/net/acoyt/acornlib/mixin/compat/trinkets/HeadFeatureRendererMixin.java
+++ src/main/java/net/acoyt/acornlib/mixin/compat/trinkets/HeadFeatureRendererMixin.java
@@ -2,13 +2,20 @@ package net.acoyt.acornlib.mixin.compat.trinkets;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.acoyt.acornlib.compat.TrinketsCompat;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
+import net.acoyt.acornlib.impl.index.tag.AcornItemTags;
 import net.minecraft.client.render.entity.feature.HeadFeatureRenderer;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Pair;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Optional;
 
 @Mixin(HeadFeatureRenderer.class)
 public class HeadFeatureRendererMixin {
@@ -22,6 +29,21 @@ public class HeadFeatureRendererMixin {
     )
     private ItemStack acornlib$changeHeadStack(LivingEntity instance, EquipmentSlot equipmentSlot, Operation<ItemStack> original) {
         ItemStack stack = original.call(instance, equipmentSlot);
-        return TrinketsCompat.hasPlushTrinket(instance) ? TrinketsCompat.getPlushTrinket(instance).orElse(stack) : stack;
+        return acornlib$getPlushTrinket(instance).orElse(stack);
+    }
+
+    @Unique
+    private static Optional<ItemStack> acornlib$getPlushTrinket(LivingEntity living) {
+        Optional<TrinketComponent> optional = TrinketsApi.getTrinketComponent(living);
+        if (optional.isEmpty()) return Optional.empty();
+
+        TrinketComponent component = optional.get();
+        for (Pair<SlotReference, ItemStack> pair : component.getEquipped(stack -> stack.isIn(AcornItemTags.PLUSHIES))) {
+            if (pair.getLeft().inventory().getSlotType().getName().equals("hat") && pair.getRight().isIn(AcornItemTags.PLUSHIES)) {
+                return Optional.of(pair.getRight());
+            }
+        }
+
+        return Optional.empty();
     }
 }
